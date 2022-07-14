891e7f8b73e78ba1eb26020716dde55182949ee0
Nong <nong@abc.com>
2022-07-14 16:29:03 +0700

    Video-Add-Routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,23 @@
-import data from "./data";
+
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">amazone</a>
-      </header>
-      <main>
-        <h1>Featured Products</h1>
-        <div className="products">
-          {data.products.map(product =>(
-           <div className="product" key={product.slug}>
-            <a href={'/product/$ {product.slug}'}>
-              <img src={product.Image} alt={product.name} />
-            </a>
-            <div className="product-info">
-              <a href={'/product/$ {produt.slug}'}>
-                <p>{product.name}</p>
-              </a>
-              <p>
-                <strong>
-                  {product.price}
-                </strong>
-              </p>
-              <button>Add to cart</button>
-            </div>
-           </div>))
-          }
-        </div>
-      </main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/">amazone</Link>
+        </header>
+        <main>
+         <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />}/>
+            <Route path="/" element= {<HomeScreen/>}/>
+         </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
